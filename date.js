cbc713e725d1e5f8e461a5c24dcfafe054c2fa83
Anonymouswinner7189 <114478424+Anonymouswinner7189@users.noreply.github.com>
2023-03-21 12:04:33 +0530

    first-commit

---
+++ date.js
@@ -0,0 +1,14 @@
+// Shortcut for module.exports is exports
+
+exports.getDate = () => {
+
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  return today.toLocaleDateString("en-US", options);
+};
